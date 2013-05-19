37c5a94c9b92a5500aa1a990a2a8fb44417ec5b6
Andrew Stamm <andrewstamm@me.com>
2013-05-19 14:16:48 -0500

    multi-user haiku blog

--- db/migrate/20130518142432_create_haikus.rb
+++ db/migrate/20130518142432_create_haikus.rb
@@ -2,6 +2,7 @@ class CreateHaikus < ActiveRecord::Migration
   def change
     create_table :haikus do |h|
       h.string :line_1, :line_2, :line_3
+      h.integer :user_id
       h.integer :votes, :default => 0
       h.timestamps
     end
